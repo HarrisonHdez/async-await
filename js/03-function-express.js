0601207c997a8d9615ca58b624bc8fc57ff7f0d3
Harrison Hdez <hhdezprogrammer@gmail.com>
2023-02-06 19:24:30 -0600

    asyng-await

---
+++ js/03-function-express.js
@@ -0,0 +1,27 @@
+function descargarClientes() {
+    
+    return new Promise((resolve, reject) => {
+        const error = true;
+
+        setTimeout(() => {
+            if(!error) {
+                resolve('El Listado de Clientes se descargo correctamente');
+            } else {
+                reject('Error en la conexion');
+            }
+        }, 3000);
+    })
+}
+
+// Async await
+const ejecutar = async () => {
+    try {
+        const respuesta = await descargarClientes();
+
+        console.log( 2 + 2 );
+        console.log(respuesta);
+    } catch (error) {
+        console.log(error);
+    }
+}    
+ejecutar();
